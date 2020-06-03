733aabc2180c09401a6159517321899b58e815fc
hccake <chengbohua@foxmail.com>
2020-06-04 00:57:20 +0800

    :hammer: 示例项目位置调整

---
+++ ballcat-sample-admin-application/src/main/java/com/your/packages/admin/modules/test/AdminDemoJobHandler.java
@@ -0,0 +1,38 @@
+package com.your.packages.admin.modules.test;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * XxlJob开发示例（Bean模式）
+ *
+ * 开发步骤：
+ * 1、在Spring Bean实例中，开发Job方法，方式格式要求为 "public ReturnT<String> execute(String param)"
+ * 2、为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ * 3、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
+ *
+ * @author xuxueli 2019-12-11 21:52:51
+ */
+@Component
+public class AdminDemoJobHandler {
+
+	@XxlJob(value="adminDemoJobHandler")
+	public ReturnT<String> execute(String param) throws Exception {
+		XxlJobLogger.log("AdminDemoJobHandler Invoke Success.");
+
+		System.out.println("执行成功！："+ LocalDateTime.now().toString());
+
+		for (int i = 0; i < 5; i++) {
+			XxlJobLogger.log("beat at:" + i);
+			TimeUnit.SECONDS.sleep(2);
+		}
+		return ReturnT.SUCCESS;
+	}
+
+}
