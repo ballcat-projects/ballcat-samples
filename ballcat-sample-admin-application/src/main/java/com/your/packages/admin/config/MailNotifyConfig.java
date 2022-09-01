e5e3f3efcae40d70c4baaaecea37184cbf75a23f
Hccake <chengbohua@foxmail.com>
2022-09-01 12:13:25 +0800

    :sparkles: 添加邮件通知的使用以及配置示例

---
+++ ballcat-sample-admin-application/src/main/java/com/your/packages/admin/config/MailNotifyConfig.java
@@ -0,0 +1,20 @@
+package com.your.packages.admin.config;
+
+import com.hccake.ballcat.common.mail.sender.MailSender;
+import com.hccake.ballcat.notify.push.MailNotifyPusher;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hccake
+ */
+@Configuration(proxyBeanMethods = false)
+public class MailNotifyConfig {
+
+	@Bean
+	@ConditionalOnBean(MailSender.class)
+	public MailNotifyPusher mailNotifyPusher(MailSender mailSender) {
+		return new MailNotifyPusher(mailSender);
+	}
+}
