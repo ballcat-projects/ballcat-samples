3653c3cb3032480804e953edf5aa7747d1112633
ace <381187081@qq.com>
2022-09-13 19:30:59 +0800

    :sparkles: 添加登录滑块验证码

---
+++ ballcat-sample-admin-application/src/main/java/com/your/packages/admin/sample/controller/TianaiCaptchaController.java
@@ -0,0 +1,46 @@
+package com.your.packages.admin.sample.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import cloud.tianai.captcha.common.constant.CaptchaTypeConstant;
+import cloud.tianai.captcha.spring.application.ImageCaptchaApplication;
+import cloud.tianai.captcha.spring.vo.CaptchaResponse;
+import cloud.tianai.captcha.spring.vo.ImageCaptchaVO;
+import cloud.tianai.captcha.validator.common.model.dto.ImageCaptchaTrack;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/captcha/tianai")
+@RequiredArgsConstructor
+public class TianaiCaptchaController {
+ 
+  private final ImageCaptchaApplication imageCaptchaApplication;
+
+    @GetMapping("/gen")
+    @ResponseBody
+    public CaptchaResponse<ImageCaptchaVO> genCaptcha(HttpServletRequest request, @RequestParam(value = "type", required = false)String type) {
+        if (StringUtils.isBlank(type)) {
+            type = CaptchaTypeConstant.SLIDER;
+        }
+        CaptchaResponse<ImageCaptchaVO> response = imageCaptchaApplication.generateCaptcha(type);
+        return response;
+    }
+
+    @PostMapping("/check")
+    @ResponseBody
+    public boolean checkCaptcha(@RequestParam("id") String id,
+                                @RequestBody ImageCaptchaTrack imageCaptchaTrack,
+                                HttpServletRequest request) {
+        return imageCaptchaApplication.matching(id, imageCaptchaTrack);
+    }
+
+}
