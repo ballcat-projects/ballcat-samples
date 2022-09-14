cd1a042a4941b16fc31a3b5b51173980cf482b8f
ace <381187081@qq.com>
2022-09-14 12:28:06 +0800

    :ambulance: 图形验证码二次校验

--- ballcat-sample-admin-application/src/main/java/com/your/packages/admin/captcha/TianaiCaptchaValidator.java
+++ ballcat-sample-admin-application/src/main/java/com/your/packages/admin/captcha/TianaiCaptchaValidator.java
@@ -1,8 +1,5 @@
 package com.your.packages.admin.captcha;
 
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Primary;
@@ -12,30 +9,23 @@ import com.hccake.ballcat.auth.filter.captcha.CaptchaValidator;
 import com.hccake.ballcat.auth.filter.captcha.domain.CaptchaResponse;
 
 import cloud.tianai.captcha.spring.application.ImageCaptchaApplication;
-import cloud.tianai.captcha.validator.common.model.dto.ImageCaptchaTrack;
+import cloud.tianai.captcha.spring.plugins.secondary.SecondaryVerificationApplication;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @Primary
 @RequiredArgsConstructor
 public class TianaiCaptchaValidator implements CaptchaValidator {
 
-  private final ImageCaptchaApplication application;
+  private final ImageCaptchaApplication sca;
 
   @Override
   public CaptchaResponse validate(HttpServletRequest request) {
-    String id = request.getParameter("id");
-    ImageCaptchaTrack imageCaptchaTrack = null;
-    try(InputStream is = request.getInputStream()){
-      ObjectInputStream ois = new ObjectInputStream(is);
-      imageCaptchaTrack = (ImageCaptchaTrack)ois.readObject();
-      ois.close();
-    }catch(Exception ex){
-      log.error("read captcha validation body failed");
+    String id = request.getParameter("captchaId");
+    boolean match = false;
+    if(sca instanceof SecondaryVerificationApplication){
+       match = ((SecondaryVerificationApplication) sca).secondaryVerification(id);
     }
-    boolean match = application.matching(id, imageCaptchaTrack);
     CaptchaResponse captchaResponse = new CaptchaResponse();
     captchaResponse.setSuccess(match);
     return captchaResponse;
