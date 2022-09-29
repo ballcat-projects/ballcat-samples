07d3ce8db4ac197508a937e0f88c57eda43954c4
Hccake <chengbohua@foxmail.com>
2022-09-29 20:34:24 +0800

    :fire: 删除 swagger 提供者演示项目的 security 相关

--- ballcat-sample-swagger-provider/src/main/java/com/hccake/ballcat/api/ApiApplication.java
+++ ballcat-sample-swagger-provider/src/main/java/com/hccake/ballcat/api/ApiApplication.java
@@ -1,12 +1,10 @@
 package com.hccake.ballcat.api;
 
-import com.hccake.ballcat.common.security.annotation.EnableOauth2ResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // @EnableXxlJob
 @SpringBootApplication
-@EnableOauth2ResourceServer
 public class ApiApplication {
 
 	public static void main(String[] args) {
