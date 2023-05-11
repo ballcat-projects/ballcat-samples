b733475c3149d80e479349314821039c3033223c
Hccake <chengbohua@foxmail.com>
2023-05-11 11:41:42 +0800

    :sparkles: 添加 excel 使用 sample

---
+++ ballcat-sample-excel/src/main/java/org/ballcat/sample/excel/DemoData.java
@@ -0,0 +1,10 @@
+package org.ballcat.sample.excel;
+
+import lombok.Data;
+
+// 实体对象
+@Data
+public class DemoData {
+	private String username;
+	private String password;
+}
---
+++ ballcat-sample-excel/src/main/java/org/ballcat/sample/excel/ExcelSampleApplication.java
@@ -0,0 +1,16 @@
+package org.ballcat.sample.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hccake
+ */
+@SpringBootApplication
+public class ExcelSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelSampleApplication.class, args);
+    }
+
+}
